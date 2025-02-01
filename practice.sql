95b3de27ffa8f60bcb9ea13cf7e043edadf3cca4
Stephen Taylor <stephenmtaylor93@gmail.com>
2025-02-01 17:11:46 -0500

    working

--- practice.sql
+++ practice.sql
@@ -227,3 +227,212 @@ UPDATE temperatures
 
 ALTER TABLE temperatures
     RENAME TO weather;
+
+
+
+
+ALTER TABLE films ALTER COLUMN title SET NOT NULL;
+ALTER TABLE films ALTER COLUMN year SET NOT NULL;
+ALTER TABLE films ALTER COLUMN genre SET NOT NULL;
+ALTER TABLE films ALTER COLUMN director SET NOT NULL;
+ALTER TABLE films ALTER COLUMN duration SET NOT NULL;
+
+
+ALTER TABLE films ADD CONSTRAINT unique_title UNIQUE (title);
+
+ALTER TABLE films DROP CONSTRAINT unique_title;
+
+
+ALTER TABLE films
+    ADD CONSTRAINT title_length CHECK (length(title) >= 1);
+
+INSERT INTO films (title, year, genre, director, duration)
+    VALUES ('', 2025, 'Thriller', 'Joe Joe', 120);
+
+
+ALTER TABLE films DROP CONSTRAINT title_length;
+
+
+ALTER TABLE films
+    ADD CONSTRAINT film_year CHECK (year BETWEEN 1900 and 2100 );
+
+
+ALTER TABLE films
+    ADD CONSTRAINT director_name CHECK (length(director) >= 3 AND director LIKE '% %');
+
+
+UPDATE films
+    SET director = 'Johnny'
+    WHERE title = 'Die Hard';
+
+
+CHECK
+DATA TYPE
+UNIQUE
+NOT NULL
+
+CREATE TABLE honkey (
+    pig varchar(10) NOT NULL DEFAULT NUll
+);
+
+INSERT INTO honkey
+    (pig)
+    VALUES ();
+
+ALTER TABLE more_colors
+    ADD COLUMN counter serial;
+
+ALTER TABLE more_colors
+    DROP COLUMN counter;
+
+CREATE SEQUENCE counter;
+
+nextval(counter)
+
+INSERT INTO more_colors 
+    (id, name)
+    VALUES (5, nextval('counter'));
+    
+DROP SEQUENCE counter;
+
+CREATE SEQUENCE counter INCREMENT 2 START 2;
+-- same thing
+CREATE SEQUENCE even_counter INCREMENT BY 2 MINVALUE 2;
+
+ALTER TABLE films
+    ADD COLUMN id serial PRIMARY KEY;
+
+INSERT INTO films
+    (title, year, genre, director, duration, id)
+    VALUES ('Pigs', 2025, 'Thriller', 'Joe Joe', 120, 3);
+
+UPDATE films
+    SET id = 3 WHERE title = 'Die Hard';
+
+
+ALTER TABLE films
+    ADD COLUMN a_value serial PRIMARY KEY;
+
+ALTER TABLE films
+    DROP CONSTRAINT films_pkey;
+
+
+
+INSERT INTO films (title, year, genre, director, duration)
+    VALUES ('Wayne''s World', 1992, 'comedy', 'Penelope Spheeris', 95),
+           ('Bourne Identity', 2002, 'espionage', 'Doug Liman', 118); 
+
+
+SELECT genre FROM films
+    GROUP BY genre;
+
+SELECT DISTINCT genre FROM films;
+
+SELECT ROUND(AVG(duration)) AS avg_duration FROM films;
+
+SELECT genre, ROUND(AVG(duration)) AS avg_duration 
+    FROM films
+    GROUP BY genre;
+
+SELECT decade, ROUND(AVG(duration)) AS avg_duration
+    FROM (
+        SELECT year, year / 10 * 10 as decade, duration FROM films) as duration_by_decade
+    GROUP BY decade
+    ORDER BY decade;
+
+SELECT year / 10 * 10 as decade, ROUND(AVG(duration))
+    FROM films
+    GROUP BY decade
+    ORDER BY decade;
+
+SELECT * FROM films
+    WHERE director LIKE 'John %';
+
+
+SELECT genre, count(*)
+    FROM films
+    GROUP BY genre
+    ORDER by count DESC;
+
+
+SELECT year / 10 * 10 as decade, genre, string_agg(title, ', ') AS films
+    FROM films
+    GROUP by decade, genre
+    ORDER by decade, genre;
+
+
+SELECT genre, sum(duration) as total_duration FROM films
+    GROUP BY genre
+    ORDER BY total_duration, genre ASC;
+
+
+CREATE TABLE names
+    (id integer, name text);
+
+INSERT INTO names
+    VALUES (1, 'Abedi'),
+(2, 'Allyssa'),
+(3, 'amy'),
+(4, 'ben'),
+(5, 'Becky'),
+(6, 'Christophe'),
+(7, 'Camilla'),
+(8, 'david'),
+(9, 'Elsa'),
+(10, 'frank'),
+(11, 'Felipe');
+
+
+
+SELECT count(*) FROM tickets;
+
+SELECT count(DISTINCT customer_id) FROM tickets;
+
+SELECT round( COUNT(DISTINCT tickets.customer_id)
+            / COUNT(DISTINCT customers.id)::decimal * 100,
+            2)
+       AS percent
+  FROM customers
+  LEFT OUTER JOIN tickets
+    ON tickets.customer_id = customers.id;
+
+
+SELECT events.name, count(tickets.event_id) 
+        AS popularity
+    FROM tickets
+    LEFT OUTER JOIN events
+        ON tickets.event_id = events.id
+    GROUP BY events.id
+    ORDER BY popularity DESC;
+
+
+SELECT c.id, c.email, COUNT(DISTINCT t.event_id) FROM customers AS c
+    INNER JOIN tickets as t
+        ON t.customer_id = c.id
+    GROUP BY c.id
+    HAVING COUNT(DISTINCT t.event_id) = 3;
+
+
+SELECT events.name AS event, events.starts_at, sections.name AS section, seats.row, seats.number AS seat
+    FROM events
+    INNER JOIN tickets
+        ON events.id = tickets.event_id
+    INNER JOIN seats
+        ON tickets.seat_id = seats.id
+    INNER JOIN sections
+        ON seats.section_id = sections.id
+    INNER JOIN customers
+        ON tickets.customer_id = customers.id
+    WHERE customers.email = 'gennaro.rath@mcdermott.co';
+
+SELECT events.name AS event, events.starts_at, sections.name AS section, seats.row, seats.number AS seat
+    FROM tickets
+    INNER JOIN events
+        ON tickets.event_id = events.id
+    INNER JOIN customers
+        ON tickets.customer_id = customers.id
+    INNER JOIN seats
+        ON tickets.seat_id = seats.id
+    INNER JOIN sections
+        ON seats.section_id = sections.id
+    WHERE customers.email = 'gennaro.rath@mcdermott.co';
